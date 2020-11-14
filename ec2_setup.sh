b2c99de64ee961b827d10a888a7efae7d0f3ae98
Nurlan Duishoev <nurlanbek.duishoev@gmail.com>
2020-11-14 21:40:45 +0100

    fix repo name

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -7,9 +7,9 @@ yes | sudo yum install python3.7
 yes | sudo yum install git
 
 # clone git repo
-git clone https://github.com/nlngh/warc_parser.git
+git clone https://github.com/nlngh/ct_warc_to_doc.git
 
-cd warc_parser/
+cd ct_warc_to_doc/
 
 sudo pip3 install -r requirements.txt
 
